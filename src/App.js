f2947c2ae3d63f4ba59b86295f82a7809371b989
Jordan Dickson <jfdickson25@gmail.com>
2025-11-14 17:24:44 -0700

    Add animations for pressed logic

--- src/App.js
+++ src/App.js
@@ -9,11 +9,21 @@ export default function App() {
     const [totals, setTotals] = useState([0, 0, 0]);
     const [rotateLogo, setRotateLogo] = useState(false);
     const [add, setAdd] = useState(true);
+    const [pressed, setPressed] = useState([false, false, false, false]);
 
     return (
         <div id="app">
             <div id="add-subtract">
-                <FontAwesomeIcon icon={add ? faPlus : faMinus} size="2x" color="white" onClick={() => setAdd(!add)} />
+                <FontAwesomeIcon icon={add ? faPlus : faMinus} size="2x" color="white" className={ pressed[3] ? "pressed" : "" }
+                    onClick={() => 
+                        {
+                            setPressed([false, false, false, true]);
+                            setTimeout(() => {
+                                setAdd(!add);
+                                setPressed([false, false, false, false]);
+                             }, 500);
+                        }
+                    } />
             </div>
             <img
                 className={rotateLogo ? "rotate" : ""}
@@ -30,7 +40,8 @@ export default function App() {
                 </div>
             </div>
             <div id="tokens">
-                <img src={`${process.env.PUBLIC_URL}/images/Moonrollers-1-Token.png`} className="token" onClick={() => 
+                <img src={`${process.env.PUBLIC_URL}/images/Moonrollers-1-Token.png`} className={pressed[0] ? "token pressed" : "token"} 
+                    onClick={() => 
                     { 
                         if (add) {
                             setPrestige(prestige + 1); setTotals([totals[0] + 1, totals[1], totals[2]]);
@@ -39,9 +50,13 @@ export default function App() {
                                 setPrestige(prestige - 1); setTotals([totals[0] - 1, totals[1], totals[2]]);
                             }
                         }
+
+                        setPressed([true, false, false, false]);
+                        setTimeout(() => setPressed([false, false, false, false]), 500);
                     }
                 } />
-                <img src={`${process.env.PUBLIC_URL}/images/Moonrollers-2-Token.png`} className="token" onClick={() => 
+                <img src={`${process.env.PUBLIC_URL}/images/Moonrollers-2-Token.png`} className={pressed[1] ? "token pressed" : "token"}
+                    onClick={() => 
                     { 
                         if (add) {
                             setPrestige(prestige + 2); setHazardSymbols(hazardSymbols + 1); setTotals([totals[0], totals[1] + 1, totals[2]]);
@@ -50,9 +65,14 @@ export default function App() {
                                 setPrestige(prestige - 2); setHazardSymbols(hazardSymbols - 1); setTotals([totals[0], totals[1] - 1, totals[2]]);
                             }
                         }
+ 
+                        setPressed([false, true, false, false]);
+
+                        setTimeout(() => setPressed([false, false, false, false]), 500);
                     }
                 } />
-                <img src={`${process.env.PUBLIC_URL}/images/Moonrollers-5-Token.png`} className="token" onClick={() => 
+                <img src={`${process.env.PUBLIC_URL}/images/Moonrollers-5-Token.png`} className={pressed[2] ? "token pressed" : "token"}
+                    onClick={() => 
                     { 
                         if (add) {
                             setPrestige(prestige + 5); setHazardSymbols(hazardSymbols + 2); setTotals([totals[0], totals[1], totals[2] + 1]); 
@@ -61,6 +81,9 @@ export default function App() {
                                 setPrestige(prestige - 5); setHazardSymbols(hazardSymbols - 2); setTotals([totals[0], totals[1], totals[2] - 1]);
                             }
                         }
+
+                        setPressed([false, false, true, false]);
+                        setTimeout(() => setPressed([false, false, false, false]), 500);
                     }
                 } />
                 <div className="count">{totals[0]}</div>
